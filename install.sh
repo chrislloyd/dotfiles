646dff5c8fe596abdae66a00141c464417f15a8d
Chris Lloyd <chris@chrislloyd.net>
2022-03-27 22:21:07 -0700

    Clean up install

--- install.sh
+++ install.sh
@@ -1,83 +1,64 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-function install() {
-  echo "Installing $1..."
-  ($2)
-}
-
-function skip() {
-  echo "Skipping"
-}
-
-function zgen() {
-	if [ -d "${HOME}/.zgen" ]; then
-    skip
-    return
-	fi
-
-  git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
-}
-
-function xcode() {
-  if [ "$(uname)" != "Darwin" ] || [ $(xcode-select -p 1>/dev/null;echo $?) ]; then
-    skip
-    return
-  fi
-
-  sudo xcode-select --install
-}
-
-function homebrew() {
-  if [ "$(uname)" != "Darwin" ]; then
-    skip
-    return
-  fi
-
-  if ! command -v brew > /dev/null; then
-    CI=true /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  fi
-
-  brew bundle --file homebrew/.Brewfile
-}
-
-# Install dependencies
-install "zgen" zgen
-install "XCode" xcode
-install "homebrew" homebrew
-
-__dirname=$(cd $(dirname $0); pwd)
+readonly DOTFILES=$(cd $(dirname $0); pwd)
 pushd $PWD
 cd
 
+# config
+mkdir -p .config
+
 # bash
-ln -s -f $__dirname/bash/.bash_profile .
-ln -s -f $__dirname/bash/.bashrc .
+ln -s -f $DOTFILES/bash/.bash_profile .
+ln -s -f $DOTFILES/bash/.bashrc .
 
 # ssh
 mkdir -p .ssh
 mkdir -p .ssh/config.d
-ln -s -f $__dirname/ssh/config .ssh
+ln -s -f $DOTFILES/ssh/config .ssh
 
 # editorconfig
-ln -s -f $__dirname/editorconfig/.editorconfig .
+ln -s -f $DOTFILES/editorconfig/.editorconfig .
 
 # emacs
-ln -s -f $__dirname/emacs/.emacs.d .
+ln -s -f $DOTFILES/emacs/.emacs.d .
 
 # git
-cp $__dirname/git/.gitconfig .
-ln -s -f $__dirname/git/.global.gitconfig .
-ln -s -f $__dirname/git/.gitignore .
-ln -s -f $__dirname/git/.gitmessage .
+cp $DOTFILES/git/.gitconfig .
+ln -s -f $DOTFILES/git/.global.gitconfig .
+ln -s -f $DOTFILES/git/.gitignore .
+ln -s -f $DOTFILES/git/.gitmessage .
 
 # shell
 touch .hushlogin
-ln -s -f $__dirname/shell/.inputrc .
-ln -s -f $__dirname/shell/.profile .
+ln -s -f $DOTFILES/shell/.inputrc .
+ln -s -f $DOTFILES/shell/.profile .
 
 # zsh
-ln -s -f $__dirname/zsh/.zprofile .
-ln -s -f $__dirname/zsh/.zshrc .
+ln -s -f $DOTFILES/zsh/.zprofile .
+ln -s -f $DOTFILES/zsh/.zshrc .
+
+# zgen
+if [ ! -d "${HOME}/.zgen" ]
+then
+  git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+fi
+
+# xcode
+if [ "$(uname)" == "Darwin" ] && [ ! $(xcode-select -p 1>/dev/null;echo $?) ]
+then
+  sudo xcode-select --install
+fi
+
+# homebrew
+if [ "$(uname)" == "Darwin" ]
+then
+  if ! command -v brew > /dev/null
+  then
+    CI=true /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
+  ln -s -f $DOTFILES/homebrew/Brewfile .Brewfile
+  brew bundle --no-upgrade --global
+fi
 
 popd
